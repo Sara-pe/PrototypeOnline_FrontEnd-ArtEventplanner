8a62701bfad9e5fb8fad64333aa35ed18f19b7ea
Sara <sara@Saras-MacBook-Pro.local>
2026-05-08 12:38:18 +0200

    correct

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const authService = {
   register: async (userData) => {
         try {
-            const response = await axios.post("http://localhost:3000/api/auth/register", userData);
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, userData);
             return response.data;
             
         } catch (error) {
@@ -18,7 +18,7 @@ const authService = {
 
     login: async ({ email, password }) => {
 
-        const response = await axios.post("http://localhost:3000/api/auth/login", { email, password })
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, { email, password })
         return response.data
     }
 }
