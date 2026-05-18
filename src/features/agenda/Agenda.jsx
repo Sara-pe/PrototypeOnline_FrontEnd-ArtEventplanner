3648a4c2c8cc3b5896d384d59e6ac9b942815c87
Sara <sara@Saras-MacBook-Pro.local>
2026-05-18 14:01:36 +0200

    update agenda

--- src/features/agenda/Agenda.jsx
+++ src/features/agenda/Agenda.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Rec1 } from './components/Rec1'
 import { Rec2 } from './components/Rec2'
 import { Rec3 } from './components/Rec3'
+import { Rec4 } from './components/Rec4'
 
 
 export const Agenda = () => {
@@ -31,9 +32,8 @@ export const Agenda = () => {
                 <div className={styles.content}>
                     <Rec3 />
                     <Rec1 />
-                    <Rec2 />
-
-          
+                     <Rec4 />  
+                     <Rec2 />
 
                 </div>
             </div>
